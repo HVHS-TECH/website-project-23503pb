80629d80ff5e8ac2909e663f4ea656c3632ef9c5
Paul Martin <borabopaul4@gmail.com>
2025-06-16 02:13:49 +1200

    (ONGOING) Improve navigation bar responsiveness; begin implementing hamburger menu

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log('%c script.js \n--------------------',
     'color: blue; background-color: white;');
+
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('hidden');
+}
